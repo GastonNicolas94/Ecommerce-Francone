88fad44f5299d5b151ee03a3d364e9fdebe76661
Gastón Nicolás Francone <gastonfrancone_2006@hotmail.com>
2022-03-04 13:21:41 -0300

    DESAFIO 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar/NavBar'
 import {default as rugido} from './rugido.png'
+import ItemListConteiner from './conteiner/ItemListConteiner';
 
 function App() {
   return (
     <div className="App">
       < NavBar />
-      <img src={rugido} className="app-imagen"/>
+      <img src={rugido} alt='' className="app-imagen"/>
+      <ItemListConteiner greeting="HOLA SOY ITEM LIST CONTEINER"/>
     </div>
   );
 }
